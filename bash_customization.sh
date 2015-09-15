27a541c45cf1e46920427d89127e594ec6a0bbc0
Asbjørn Alexander Fellinghaug <asbjorn@fellinghaug.com>
2015-09-15 08:44:44 +0200

    updating my own ~/.bashrc customizations

--- bash_customization.sh
+++ bash_customization.sh
@@ -2,44 +2,14 @@
 GIT_PS1_SHOWDIRTYSTATE=true
 export PS1="\[\033[01;32m\]\u@\h\$(__git_ps1)\[\033[00m\]:\[\033[01;34m\]\w\[\033[00m\]\$ "
 
-# Function to output a random_key. Default is a 32 item length, while 
-# you may specify another length as an argument
-random_key () {
-    if [[ -z "$1" ]]; then
-        RANDOM_LEN=32
-    else
-        RANDOM_LEN=$1
-    fi
-    python -c "import os;print os.urandom($RANDOM_LEN)"
-}
+# The one and only <3
+export EDITOR=vim
 
-# Reset proxy settings
-function noproxy() {
-    export http_proxy=""
-    export https_proxy=""
-    export ftp_proxy=""
-    export socks_proxy=""
-    echo "Nullify proxy settings"
-}
+# User install path
+export PATH=$HOME/.local/bin:$PATH
+export LD_LIBRARY_PATH=$HOME/.local/lib:$LD_LIBRARY_PATH
 
-# Set proxy settings
-function setproxy() {
-    local company_proxy=http://www-proxy.company.com:80
-    export http_proxy=$company_proxy
-    export https_proxy=$company_proxy
-    export ftp_proxy=$company_proxy
-    export socks_proxy=$company_proxy
-}
+# Go
+export GOPATH=$HOME/.local/goprojects
+export PATH=$PATH:$GOPATH/bin
 
-# go up N directories
-function up()
-{
-    local str=""
-    local count=0
-    while [[ "$count" -lt "$1" ]] ;
-    do
-        str=$str"../"
-        let count=count+1
-    done
-    cd $str
-}
