83f9664bd0e527dad41e855b44f6e55473352b56
Asbjørn Alexander Fellinghaug <asbjorn@fellinghaug.com>
2015-10-02 14:10:36 +0200

    updated installer to get a confirmation to whether install dotfiles or not

--- install.sh
+++ install.sh
@@ -11,6 +11,38 @@ test -d $INSTALLDIR || mkdir $INSTALLDIR
 # Create backup dir if not already created
 test -d $BACKUPDIR || mkdir $BACKUPDIR
 
+ask() {
+    # http://djm.me/ask
+    while true; do
+
+        if [ "${2:-}" = "Y" ]; then
+            prompt="Y/n"
+            default=Y
+        elif [ "${2:-}" = "N" ]; then
+            prompt="y/N"
+            default=N
+        else
+            prompt="y/n"
+            default=
+        fi
+
+        # Ask the question - use /dev/tty in case stdin is redirected from somewhere else
+        read -p "$1 [$prompt] " REPLY </dev/tty
+
+        # Default?
+        if [ -z "$REPLY" ]; then
+            REPLY=$default
+        fi
+
+        # Check if the reply is valid
+        case "$REPLY" in
+            Y*|y*) return 0 ;;
+            N*|n*) return 1 ;;
+        esac
+
+    done
+}
+
 link() {
     echo "Link '$HOME/.$1'"
     if [ ! -h $HOME/.$1 ]; then
@@ -30,24 +62,35 @@ link() {
     fi
 }
 
-# VIM
-echo "init vim >>>"
-link vimrc
-link gvimrc
-if [ ! -d vim/bundle/vundle ]; then
-    git clone https://github.com/gmarik/vundle.git vim/bundle/vundle
+echo "This will install these 'dotfiles' to your \$HOME environment."
+echo "It will take a backup of everything to '$BACKUPDIR', but "
+echo "be aware that many things will likely change."
+echo 
+
+if ask "Are you sure you want to continue?"; then
+    # VIM
+    echo "init vim >>>"
+    link vimrc
+    link gvimrc
+    if [ ! -d vim/bundle/vundle ]; then
+        git clone https://github.com/gmarik/vundle.git vim/bundle/vundle
+    fi
+    link vim
+    
+    # Git specific
+    link gitconfig
+    link gitmessage
+    
+    # tmux
+    link tmux.conf
+    
+    # Shell
+    link bashrc
+    link bash_aliases
+    link bash_functions.sh
+    link bash_customization.sh
+else
+    echo "Quiting."
+    echo "Haven't touched a thing! Pinky-swear!"
 fi
-link vim
 
-# Git specific
-link gitconfig
-link gitmessage
-
-# tmux
-link tmux.conf
-
-# Shell
-link bashrc
-link bash_aliases
-link bash_functions.sh
-link bash_customization.sh
